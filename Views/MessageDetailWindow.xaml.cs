d0245b7a55981d52756432355f5c86927ceefac1
vince <vince.leroy@outlook.com>
2022-01-07 13:38:54 +0100

    MessageDetailWindow created - MessageDetailViewModel updated (display informations)

---
+++ Views/MessageDetailWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using devTalksWPF.Classes;
+using devTalksWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace devTalksWPF.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour MessageDetailWindow.xaml
+    /// </summary>
+    public partial class MessageDetailWindow : Window
+    {
+        public MessageDetailWindow(Message message)
+        {
+            InitializeComponent();
+            DataContext = new MessageDetailViewModel(message);
+        }
+    }
+}
